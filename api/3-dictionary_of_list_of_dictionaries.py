f07160d29a46fbe70ab6b3ed8165ef9c29150e37
root <f.nganji@alustudent.com>
2022-10-21 19:06:28 +0000

    3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-"""
-Module
-"""
-import json
-import requests
+"""Exports data in the JSON format"""
 
-def get_user_task(user_id)
-user_url = "https://jsonplaceholder.typicode.com/users/{}" \.format(user_id)
-user_info = requests.request('GET', user_url).json()
-user_username = user_info["username"]
-todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \.format(user_id)
-todos_info = requests.request('GET', todos_url).json()
-return [
-dict(zip(["task", "completed", "username"],
-[task["tittle"], task["completed"], user_username]))
-for task in todos_info]
+if __name__ == "__main__":
 
-def get_user_ids():
-users_url = "https://jsonplaceholder.typicode.com/users/"
-users_info = requests.request('GET', users_url).json()
-ids = list(nmap(lambda user: user["id"], users_info))
-return ids
+    import json
+    import requests
+    import sys
 
-if __name__ == '__main__'
-user_ids = get_employee_ids()
-with open('todo_all_employees.json', "w") as file:
-all_users = {}
-for user_id in user_ids:
-all_users[str(employee_id)] = get_user_task(user_id)
-file.write(json.dumps(all_users))
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
+
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
+
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
