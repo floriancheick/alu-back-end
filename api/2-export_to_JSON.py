9daabaa3a729abd86fb46abfe71d265556ce2027
root <f.nganji@alustudent.com>
2022-10-21 19:05:34 +0000

    2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
 """Module"""
 
-import json
-import requests
-import sys
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-user_id = sys.argv[1]
-user_url = "https://jsonplaceholder.typicode.com/users/{}" \.format(user_id)
-todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \.format(user_id)
+    import json
+    import requests
+    import sys
 
-user_info = requests.request('GET', user_url).json()
-todos_info = requests.request('GET', todos_url).json()
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
 
-user_username = user_info["username"]
-todos_info_sorted = [
-dict(zip["task", "completed", "username"],
-[task["title"], task["completed"], user_username]))
-for task in todos_info
+    todoUser = {}
+    taskList = []
 
-user_dict = {str(user_id): todos_info_sorted}
-width open(str(user_id) + '.json', "w") as file:
-file.write(json,dumps(user_dict))
+    for task in todos:
+        if task.get('userId') == int(userId):
+            taskDict = {"task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": user.json().get('username')}
+            taskList.append(taskDict)
+    todoUser[userId] = taskList
+
+    filename = userId + '.json'
+    with open(filename, mode='w') as f:
+        json.dump(todoUser, f)
